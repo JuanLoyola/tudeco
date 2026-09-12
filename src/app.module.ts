285a0b4e57b67497e48ac43e89feb85b7da84040
JLoyola <juan.loyola1@hotmail.com>
2026-09-12 17:48:32 -0300

    setting prisma and articles

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ArticlesModule } from './articles/articles.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +15,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'tudeco',
     }),
+    ArticlesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
