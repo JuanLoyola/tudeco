5dcc27c52b85be7b219acace2be45df01d12d4c3
JLoyola <juan.loyola1@hotmail.com>
2026-09-12 20:33:47 -0300

    Add PrismaModule to AppModule and integrate PrismaService in ArticlesService for database access

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ArticlesModule } from './articles/articles.module.js';
+import { PrismaModule } from '../prisma/prisma.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -16,6 +17,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       serviceId: 'tudeco',
     }),
     ArticlesModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service.js'
 
 @Injectable()
-export class ArticlesService {}
+export class ArticlesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.articles.findMany()
+  }
+}
